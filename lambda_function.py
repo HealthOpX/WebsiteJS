d7094b51d51cb61a00b1a2fae23b4ce363db0ca8
HealthOpX <47020883+HealthOpX@users.noreply.github.com>
2019-06-24 16:56:19 -0400

    Add files via upload
    Files used in determining chatbot logic

---
+++ lambda_function.py
@@ -0,0 +1,216 @@
+#Notes for the 06_25_19 me
+
+#   Should seperate this lambda_function file from checkup_intent.py/guide_intent.py
+
+#   For example have these functions in lambda_function file:
+#       execute query, get_slots, close, build_validation_result
+#   Everything else should be under the specific intent file.
+#   This file should have 
+
+"""
+This sample demonstrates an implementation of the Lex Code Hook Interface
+in order to serve a sample bot which manages orders for flowers.
+Bot, Intent, and Slot models which are compatible with this sample can be found in the Lex Console
+as part of the 'OrderFlowers' template.
+
+For instructions on how to set up and test this bot, as well as additional samples,
+visit the Lex Getting Started documentation http://docs.aws.amazon.com/lex/latest/dg/getting-started.html.
+"""
+import math
+import dateutil.parser
+import datetime
+import time
+import os
+import logging
+
+import guide_intent
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+""" --- Helpers to build responses which match the structure of the necessary dialog actions --- """
+
+def execute_query(query):
+    pass
+
+
+def get_slots(intent_request):
+    """
+    Helper function, returns slots
+    """
+    return intent_request["currentIntent"]["slots"]
+
+def close(session_attributes, fulfillment_state, message):
+    response = {
+        'sessionAttributes': session_attributes,
+        'dialogAction': {
+            'type': 'Close',
+            'fulfillmentState': fulfillment_state,
+            'message': message
+        }
+    }
+
+    return response
+
+def build_validation_result(is_valid, violated_slot, message_content):
+    if message_content is None:
+        return {
+            "isValid": is_valid,
+            "violatedSlot": violated_slot,
+                }
+
+    return {
+        'isValid': is_valid,
+        'violatedSlot': violated_slot,
+        'message': {'contentType': 'PlainText', 
+                    'content': message_content}
+            }
+    
+    
+def validate_response(want_help):
+    if want_help != None:
+        if want_help.lower() == "yes" or want_help.lower() == 'y':
+            return build_validation_result(True, "WantHelp", "How may I help you?")
+            pass
+        
+        elif want_help.lower() == "no" or want_help.lower() == 'n':
+            #Some functionality to close the window,
+            #  If put right here then will close the window before message?
+            return build_validation_result(True, "WantHelp", "Sounds good, let me know if you need anything!")
+            pass
+        
+        else:
+            return build_validation_result(False, "WantHelp", "Please enter yes, no y or n to respond")
+            pass
+        
+    else:
+        return build_validation_result(False, "WantHelp", "Would you like help with something?")
+
+
+    
+def check_up(intent_request):
+    #logger.debug()
+    
+    #get_slots
+    #source -> DialogCodeHook or what?
+    source = intent_request["invocationSource"] 
+    #DialogCodeHook or FulfillmentCodeHook
+    
+    session_attributes = intent_request["sessionAttributes"]
+    intent_name = intent_request["currentIntent"]["name"]
+    
+    #fulfillmentState: Fulfilled or Failed
+    if source == "DialogCodeHook":
+        slots = get_slots(intent_request)
+        want_help = slots["WantHelp"]
+        
+        validation_result = validate_response(want_help)
+        
+        if not validation_result["isValid"]: #Continue if isValid equals False
+            message = validation_result["message"]
+        #    pass
+
+        # return close(intent_request['sessionAttributes'],
+        #      'Fulfilled',
+        #      {'contentType': 'PlainText',
+        #       'content': 'CheckUp'})
+            return {
+                'sessionAttributes': session_attributes,
+                'dialogAction': {
+                    'type': "ElicitSlot",
+                    'intentName': intent_name,
+                    'slots': slots,
+                    'slotToElicit': "WantHelp",
+                    'message': message
+                } 
+            }
+            
+        #Change latter to function parameter being returned
+        if want_help == 'n' or "no":
+            
+            validation_result = validate_response(want_help)
+            message = validation_result["message"]
+            #message = {'contentType': 'PlainText',
+            #              'content': 'CheckUp'}
+            return close(
+                        intent_request['sessionAttributes'],
+                        'Fulfilled',
+                        message)
+                 
+                 
+        elif want_help == 'y' or "yes":
+            message = validation_result["message"]
+            return {
+                    'sessionAttributes': session_attributes,
+                    'dialogAction': {
+                        'type': "ElicitIntent",
+                        'intentName': intent_name,
+                        'message': message
+                                    }                 
+                    }
+            
+
+""" --- Intents --- """
+
+
+def dispatch(intent_request):
+    """
+    Called when the user specifies an intent for this bot.
+    """
+
+    logger.debug('dispatch userId={}, intentName={}'.format(intent_request['userId'], intent_request['currentIntent']['name']))
+
+    intent_name = intent_request['currentIntent']['name']
+
+    if intent_name == "CheckUp":
+        #Give a question asking if the user needs help
+        
+        #Then, if the user reponds No, set this to fulfilled and have a leave message
+        #   Future functionality add integration with site of disapearing thing.
+        
+        #If user repsonds yes, then call Guide intent
+        return check_up(intent_request)
+        
+        '''
+        return close(intent_request['sessionAttributes'],
+             'Fulfilled',
+             {'contentType': 'PlainText',
+              'content': 'CheckUp'})
+        '''
+        
+    elif intent_name == "Guide":
+        return guide_intent.guide(intent_request)
+        
+        '''
+        return close(intent_request['sessionAttributes'],
+             'Fulfilled',
+             {'contentType': 'PlainText',
+              'content': 'Guide'})        
+        '''
+        
+    else:    
+    # Dispatch to your bot's intent handlers
+        return close(intent_request['sessionAttributes'],
+                     'Fulfilled',
+                     {'contentType': 'PlainText',
+                      'content': 'Not quite bub'})
+
+
+
+""" --- Main handler --- """
+
+
+def lambda_handler(event, context):
+    """
+    Route the incoming request based on intent.
+    The JSON body of the request is provided in the event slot.
+    """
+    # By default, treat the user request as coming from the America/New_York time zone.
+    os.environ['TZ'] = 'America/New_York'
+    time.tzset()
+    
+    #Bots name from bot json 
+    logger.debug('event.bot.name={}'.format(event['bot']['name']))
+
+    return dispatch(event)
