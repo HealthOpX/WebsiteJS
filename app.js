89f8968bdefab7c3657f71957cc28b7339db2c87
JoseHernandez9094 <josexavi06@gmail.com>
2019-07-07 20:56:22 -0400

    readded the db connection

--- app.js
+++ app.js
@@ -20,22 +20,22 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
 // Connection to the database!
-// var connection = mysql.createConnection(
-// {
-//   host     : process.env.RDS_HOSTNAME,
-//   user     : process.env.RDS_USERNAME,
-//   password : process.env.PASSWORD,
-//   port     : process.env.PORT,
-// });
+var connection = mysql.createConnection(
+{
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.PASSWORD,
+  port     : process.env.PORT,
+});
 
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error('Database connection failed: ' + err.stack);
-//     return;
-//   }
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
 
-//   console.log('Connected to database.');
-// });
+  console.log('Connected to database.');
+});
 
 //Code to return defult main page!
 app.get('/', function (req, res) 
