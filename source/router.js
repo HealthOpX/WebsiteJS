93e20187db42cc9542e36577c32fe1aa635445d6
JoseHernandez9094 <josexavi06@gmail.com>
2019-08-13 14:47:19 -0700

    s

--- source/router.js
+++ source/router.js
@@ -75,7 +75,7 @@ router.get('/patient.html', function (req, res)
     {
       console.log('Sucessful Post Request From API');
       console.log('res.body:', res.body);
-\      console.log('res.body.id_token:', res.body.id_token);
+      console.log('res.body.id_token:', res.body.id_token);
 
   });
 
