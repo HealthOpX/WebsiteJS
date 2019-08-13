4c77f48e103734fefb9b12e37c4185bfecf124bb
JoseHernandez9094 <josexavi06@gmail.com>
2019-08-13 14:17:23 -0700

    accessing id_token

--- source/router.js
+++ source/router.js
@@ -75,7 +75,7 @@ router.get('/patient.html', function (req, res)
     function (err, res, body) 
     {
       console.log('Sucessful Post Request From API');
-      console.log('res.body:', res.body);
+      console.log('res.body.id_token:', res.body.id_token);
   });
 
 
